2e862b46173f68035cc6cae5a4ff74790d5f7f9d
elgijb <andrejelgin8@gmail.com>
2024-08-09 18:48:23 +0300

    обнова с енв и фактами

--- main.py
+++ main.py
@@ -1,65 +1,63 @@
 import logging
-import requests
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+import os
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, CallbackContext
+import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+from animal_facts import animals
+from dotenv import load_dotenv
 
-# Включение логирования для отладки и отслеживания ошибок
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+load_dotenv()
 WIKIPEDIA_BASE_URL = "https://ru.wikipedia.org"
 
 def get_animal_info(animal_name):
-    """
-    Получает первые четыре параграфа и одно изображение о животном из Википедии.
-    
-    :param animal_name: Название животного для поиска.
-    :return: Словарь с информацией о животном и изображением или None, если страница не найдена.
-    """
-    try:
-        search_url = f"{WIKIPEDIA_BASE_URL}/w/index.php"
-        params = {"search": animal_name, "title": "Special:Search", "go": "Go"}
-        response = requests.get(search_url, params=params)
-        
-        if response.status_code != 200:
-            logger.error(f"Ошибка при запросе страницы: {response.status_code}")
-            return None
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Получение первых четырех параграфов текста
-        paragraphs = soup.find_all('p', limit=4)
-        summary = "\n\n".join([p.get_text(strip=True) for p in paragraphs]) if paragraphs else "Описание не найдено."
-
-        # Получение первого изображения из og:image
-        og_image_tag = soup.find('meta', property='og:image')
-        image = og_image_tag['content'] if og_image_tag else None
-
-        # Получение заголовка страницы
-        title = soup.find('h1', id="firstHeading").get_text(strip=True)
-
-        info = {
-            "summary": summary,
-            "url": response.url,
-            "image": image,
-            "title": title
-        }
-        return info
-    except Exception as e:
-        logger.error(f"Ошибка при получении информации о животном: {e}")
+    search_url = f"{WIKIPEDIA_BASE_URL}/w/index.php"
+    params = {"search": animal_name, "title": "Special:Search", "go": "Go"}
+    response = requests.get(search_url, params=params)
+    if response.status_code != 200:
+        logger.error(f"Ошибка при запросе страницы: {response.status_code}")
         return None
+    soup = BeautifulSoup(response.text, 'html.parser')
+    summary_paragraphs = soup.find_all('p', limit=4)
+    summary = ' '.join([p.get_text(strip=True) for p in summary_paragraphs])
+    og_image_tag = soup.find('meta', property='og:image')
+    image = og_image_tag['content'] if og_image_tag else None
+    info = {
+        "summary": summary,
+        "url": response.url,
+        "image": image,
+        "title": soup.find('h1', id="firstHeading").get_text(strip=True)
+    }
+    return info
+
+def get_animal_of_the_day():
+    day_of_month = datetime.now().day - 1
+    animal = animals[day_of_month]
+    search_url = f"{WIKIPEDIA_BASE_URL}/w/index.php"
+    params = {"search": animal['name'], "title": "Special:Search", "go": "Go"}
+    response = requests.get(search_url, params=params)
+    if response.status_code != 200:
+        logger.error(f"Ошибка при запросе страницы: {response.status_code}")
+        return None
+    soup = BeautifulSoup(response.text, 'html.parser')
+    og_image_tag = soup.find('meta', property='og:image')
+    image = og_image_tag['content'] if og_image_tag else None
+    info = {
+        "name": animal['name'],
+        "fact": animal['fact'],
+        "image": image,
+        "url": response.url
+    }
+    return info
 
 async def start(update: Update, context: CallbackContext) -> None:
-    """
-    Обработчик команды /start, отображает клавиатуру с опциями.
-    
-    :param update: Обновление, содержащее информацию о сообщении.
-    :param context: Контекст, содержащий данные о команде.
-    """
     keyboard = [
         [
             InlineKeyboardButton("Поиск по названию животного", callback_data='search_animal'),
@@ -67,7 +65,6 @@ async def start(update: Update, context: CallbackContext) -> None:
         ],
         [InlineKeyboardButton("Поиск по среде обитания", callback_data='search_habitat')]
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
         'Привет! Я ЗооИнфо бот. Выберите, что вы хотите искать:',
@@ -75,16 +72,8 @@ async def start(update: Update, context: CallbackContext) -> None:
     )
 
 async def button(update: Update, context: CallbackContext) -> None:
-    """
-    Обработчик нажатий на кнопки в инлайн-клавиатуре.
-    
-    :param update: Обновление, содержащее информацию о нажатой кнопке.
-    :param context: Контекст, содержащий данные о нажатии.
-    """
     query = update.callback_query
-    await query.answer()  # Подтверждение получения запроса
-
-    # Определение типа поиска на основе нажатой кнопки
+    await query.answer()
     if query.data == 'search_animal':
         context.user_data['search_type'] = 'animal'
         await query.edit_message_text(text="Введите название животного:")
@@ -96,53 +85,53 @@ async def button(update: Update, context: CallbackContext) -> None:
         await query.edit_message_text(text="Введите название среды обитания:")
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
-    """
-    Обработчик текстовых сообщений, осуществляет поиск по введенному запросу.
-    
-    :param update: Обновление, содержащее информацию о сообщении.
-    :param context: Контекст, содержащий данные о сообщении.
-    """
     user_input = update.message.text.strip().title()
-
     if context.user_data.get('search_type') in ['animal', 'breed']:
         info = get_animal_info(user_input)
         if info:
-            response = f"Название: {info['title']}\n\n{info['summary']}\n\nЧитать больше: {info['url']}"
+            response = f"**Название**: *{info['title']}*\n\n{info['summary']}\n\nЧитать больше: {info['url']}"
             if info['image']:
-                media = InputMediaPhoto(media=info['image'])
-                await update.message.reply_photo(photo=media.media)  # Отправляем одно изображение
-            await update.message.reply_text(response)
+                await update.message.reply_photo(photo=info['image'])
+            await update.message.reply_text(response, parse_mode='Markdown')
         else:
-            await update.message.reply_text('Извините, я не нашел информацию об этом животном. '
-                                            'Возможно, вы допустили ошибку в написании названия или такой породы нет.')
+            await update.message.reply_text('Извините, я не нашел информацию об этом животном.')
+    elif context.user_data.get('search_type') == 'habitat':
+        animals = get_habitat_info(user_input)
+        if animals:
+            response = f'Животные, найденные в среде обитания "{user_input}":\n\n'
+            for animal in animals:
+                response += f"**Название**: *{animal['title']}*\n{animal['summary']}\n\nЧитать больше: {animal['url']}\n\n"
+                if animal['images']:
+                    await update.message.reply_photo(photo=animal['images'][0])
+            await update.message.reply_text(response, parse_mode='Markdown')
+        else:
+            await update.message.reply_text(f'Извините, я не нашел информацию о среде обитания "{user_input}".')
+
+async def animal_of_the_day(update: Update, context: CallbackContext) -> None:
+    info = get_animal_of_the_day()
+    if info:
+        response = f"**Животное дня**: *{info['name']}*\n\nИнтересный факт: {info['fact']}\n\nЧитать больше: {info['url']}"
+        if info['image']:
+            await update.message.reply_photo(photo=info['image'])
+        await update.message.reply_text(response, parse_mode='Markdown')
+    else:
+        await update.message.reply_text('Извините, не удалось получить информацию о животном дня.')
 
 def main() -> None:
-    """
-    Основная функция для запуска бота.
-    """
-    # Вставьте сюда токен вашего бота
-    TOKEN = '7310780555:AAFwdne8UHZEWPBobQM7ER830ymW3J0gv2g'
-
+    TOKEN = os.getenv('TELEGRAM_TOKEN')
     if not TOKEN:
-        logger.error("Токен бота не указан. Проверьте переменную TOKEN.")
+        logger.error("Токен бота не указан. Проверьте файл .env.")
         return
-
     try:
-        # Создание и настройка бота с отключенными обработчиками сигналов
         application = Application.builder().token(TOKEN).build()
-
-        # Добавление обработчиков команд и сообщений
         application.add_handler(CommandHandler("start", start))
         application.add_handler(CallbackQueryHandler(button))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-
-        # Запуск бота
+        application.add_handler(CommandHandler("animaloftheday", animal_of_the_day))
         application.run_polling(allowed_updates=Update.ALL_TYPES, stop_signals=None)
     except Exception as e:
         logger.error(f"Ошибка при запуске бота: {e}")
 
 if __name__ == '__main__':
     main()
-
-
 
